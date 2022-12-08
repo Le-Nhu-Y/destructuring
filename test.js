46568b7b75ae75151ece0fc4b15762753a4ac4b8
Le-Nhu-Y <lenhuy20021989@gmail.com>
2022-12-08 22:09:33 +0700

    Add test.ts file

---
+++ test.js
@@ -0,0 +1,9 @@
+// let marks = {x: 21, y: -34, z: 47 };
+// const { x, y, z } = marks; // x = 21, y = -34, z = 47
+// console.log(x);
+// console.log(y);
+// console.log(z);
+var profile = { fname: 'Code', lname: 'Gym', bday: new Date('1979-01-02') };
+var fname = profile.fname, lname = profile.lname;
+console.log(fname);
+console.log(lname);
---
+++ test.ts
@@ -0,0 +1,6 @@
+
+let profile = {fname: 'Code', lname: 'Gym', bday: new Date('1979-01-02')}
+const{fname, lname} = profile;
+console.log(fname);
+console.log(lname);
+
